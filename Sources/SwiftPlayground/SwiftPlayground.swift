282d7bfd7ab6662191cd95c303b678bb4eccec7a
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-05-14 02:58:57 +0000

    e

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,5 +5,19 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
+let firstname = "Anahera"
+print(firstname)
+
+var lastname = "Smith"
+print(lastname)
+
+print("\(firstname) got married!")
+lastname = "Jones"
+print(lastname)
+
+let greeting = "Please welcome \(firstname) \(lastname)"
+print(greeting)
+
+
     }
 }
