fe26155055a259a3f1c56803ddbb3660a3cf6a26
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-05-17 22:50:35 +0000

    committed

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,17 +4,23 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!
 
+let y = 10
+let x = 14
+ 
+let Added = y + x
+let Subtracted = x - y
+let Multiplied = y * x
+let Divided = x / y
+let Remainder = x % y
+        print("hello world")
 
-
-
-
-
-
-
-
-
+print("\(y) + \(x) = \(Added)")
+print("\(x) - \(y) = \(Subtracted)")
+print("\(y) * \(x) = \(Multiplied)")
+print("\(x) / \(y) = \(Divided)")
+print("\(x) % \(y) = \(Remainder)")
+print("(\(x) + \(y)) + \(x) - \(y) = \(Added) + \(Subtracted)")
 
 
 
