29ea2af790aba1fd2fcba46119ed05b663abd453
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-05-23 22:33:27 +0000

    ME WHEN WHEN ME WHEN E ME WHEN WHEN ME WHEN WHE THE WHEN ME

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,25 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+for days in (15...30) {print (days)}
+
+for leapyear in stride (from:2000, to:2026, by: 4){
+print(leapyear)
+}
+for number in (0...100).reversed() {
+if number % 3 == 0{
+print("\(number) does not have a remainder")
+} else {
+    print("\(number) has a remainder of \(number % 3)")
+}
+}
+
+
+
+
+
+
+
     }
 }
