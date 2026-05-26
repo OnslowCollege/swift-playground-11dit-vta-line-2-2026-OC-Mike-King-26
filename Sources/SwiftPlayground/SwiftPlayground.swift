b92e23bc7c4960f2a26e7aacdbfeae985220ad1f
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-05-26 21:21:13 +0000

    absoulute cinema

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,50 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-    }
+    
+var isrunning = true
+
+print("Welcome to Cinema Tickets.")
+while isrunning {
+
+
+print("")
+print("c for child:  $8")
+print("a for adult:  $12")
+print("s for senior: $10")
+print("q for quit")
+print("please enter a choice:")
+let userInput = readLine()! .lowercased()
+var count = Int()
+switch userInput {
+
+case "c": count = count + 8 
+print("Child added, your total cost is now $\(count)")
+
+case "a": count = count + 12
+print("Adult added, your total cost is now $\(count)")
+
+case "s": count = count + 10
+print("Adult added, your total cost is now $\(count)")
+
+case "q": isrunning = false 
+
+default: print("Invalid input, please try again")
 }
+print("Your total is $\(count)")
+print("Have a good day")
+
+
+
+
+}
+    
+    
+    
+
+
+
+
+
+
+}}
