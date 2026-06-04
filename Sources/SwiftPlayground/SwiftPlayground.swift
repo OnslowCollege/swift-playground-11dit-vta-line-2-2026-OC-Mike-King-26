87e63dc6e0f8fb46db687502568b13a860da58ef
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-06-04 02:16:18 +0000

    'tis but a test

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,26 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+
+print("What is your age?")
+
+if let input = readLine(), let age = Int(input) {
+    if age <= 0 || age >= 122 {
+        print("Age incorrect.")
+    } else if age <= 18 {
+        print("Too young.")
+    } else {
+        print("You may enter")
+    }
+} else {
+    print("Please enter a number.")
+}
+
+
+
+
+
+
     }
 }
