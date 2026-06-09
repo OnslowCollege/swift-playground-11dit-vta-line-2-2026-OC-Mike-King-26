749f5f4d29b6be3f433e7eda3bfd609b33442f91
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-06-09 23:43:20 +0000

    cringe

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,46 +5,59 @@
 struct SwiftPlayground {
     static func main() {
 
-//Most constants and variables which will be used within the code are here
+//Some constants and variables which will be used within the code are here
 
-///boolean variable for controlling the while loop
-var isRunning: Bool = true
+///boolean variables for controlling whether the while loops are on or off
+var isRunning1: Bool = true
+var isRunning2: Bool = true
 
 ///constant for all the different bird species available
 let birdSpecies: [String] = [ "Tieke","Kākā", "Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī", "Kererū"]
 
-
 //makes function for printing birdSpecies list with numbers as options
 func printBirdOptions() {
-for (index, bird) in birdSpecies.enumerated() {
-    print("\(index + 1). \(bird)")
-}}
-
+    for (index, bird) in birdSpecies.enumerated() {
+        print("\(index + 1). \(bird)")
+        }
+}
 
 
 ///constant for all the different insect species available
 let insectSpecies: [String] = [ "Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko", "Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
 
-
 //makes function for printing insectSpecies list with numbers as options
 func printInsectOptions() {
-for (index, insect) in insectSpecies.enumerated() {
-    print("\(index + 1). \(insect)")
-
+    for (index, insect) in insectSpecies.enumerated() {
+        print("\(index + 1). \(insect)")
+        }
 }
 
-//testing purposes
-printBirdOptions()
-printInsectOptions()
 
-
-//note to self: remove numbered lists and make them functions instead
 
 
 //welcomes the user to the program
+print("Welcome to Zealandia Tracker.")
+print("")
 
 
-//asks what they saw if they saw anything
+//asks if they saw a bird or insect if they saw anything
+//then puts the input into a constant
+print("Did you see a bird, insect:")
+while isRunning1 {
+let userInput = readLine()?.lowercased()
+
+if userInput == "bird" {}
+
+}
+
+
+
+
+
+//makes a repeating loop which continues untill user stops seeing any more insects or birds
+
+
+//checks if the input is equal to a bird, an insect, or no
 
 
 //asks if the user saw any more species
