bbb2babd6721164f313663bc06cd96e59c79c7b2
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-06-09 21:27:14 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,38 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+//Most constants and variables which will be used within the code
+
+///variable for controlling the while loop
+var isRunning = true
+
+///constant for all the different bird species available
+let birdSpecies: [String] = [ "Tieke","Kākā", "Takahē", "Hihi", "Kiwi", "Pāteke", "Tūī", "Kererū"]
+
+///constant for all the different insect species available
+let insectSpecies: [String] = [ "Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko", "Pūngāwerewere", "Ngaokeoke", "Waemano", "Kapowai"]
+
+
+//welcomes the user to the program
+
+
+//asks what they saw if they saw anything
+
+
+//asks if the user saw any more species
+
+
+//prints what species the user saw and ends the program
+
+
+
+
+
+
+
+
+
+
     }
 }
