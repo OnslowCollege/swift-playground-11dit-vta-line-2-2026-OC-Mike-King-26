da054dbd42fa8cee7b3e8bac78b6d7524ff1d40e
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-06-14 23:12:25 +0000

    not done

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,16 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+print("Welcome to the sleep tracker.")
+print("")
+print("Let's start by recording your sleep")
+
+var HoursOfSleepEachDay: [Int] = [0, 0, 0, 0, 0, 0, 0]
+let DaysOfWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+var TotalHours = 0
+let AverageSleep = TotalHours / 7
+
+
     }
 }
