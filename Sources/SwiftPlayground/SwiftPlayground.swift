d778bfcc561704d83c40102f278a251f1bf723a8
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-06-15 01:42:43 +0000

    getting there

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -10,9 +10,33 @@ print("")
 print("Let's start by recording your sleep")
 
 var HoursOfSleepEachDay: [Int] = [0, 0, 0, 0, 0, 0, 0]
-let DaysOfWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let DayOfWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 var TotalHours = 0
 let AverageSleep = TotalHours / 7
+var IsRunning = true
+var IsRunning2 = true
+
+while IsRunning {
+    while IsRunning2 {
+    print("How many hours of sleep did you get on \(DayOfWeek)")
+    print("(To the nearest hour)")
+    let UserInput = readLine() ?? ""
+    let UserIntInput = Int(UserInput) ?? -1
+
+    if UserIntInput <= -1 {
+        print("Please make sure to enter a valid positive number.")
+    }
+    else if UserIntInput > 24 {
+    print("There are more than \(UserIntInput)")   
+    }
+    }
+
+
+//note, turn ints into double so if user inputs 24.1 hours it says there are not that many hours in a day instead of it equaling to 24
+}
+
+
+
 
 
     }
