220f7868ab33d21e8a3fb2195115cc6364005af5
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-06-16 01:53:43 +0000

    need comments

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -9,31 +9,69 @@ print("Welcome to the sleep tracker.")
 print("")
 print("Let's start by recording your sleep")
 
-var HoursOfSleepEachDay: [Int] = [0, 0, 0, 0, 0, 0, 0]
-let DayOfWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-var TotalHours = 0
-let AverageSleep = TotalHours / 7
-var IsRunning = true
-var IsRunning2 = true
+// a bunch of variable which are being used by the program
+var whichDay = 0
+var hoursOfSleepEachDay: [Double] = [0, 0, 0, 0, 0, 0, 0]
+let dayOfWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let totalHours = hoursOfSleepEachDay.reduce(0, +)
+let averageSleep = totalHours / 7
+var isRunning = true
 
-while IsRunning {
-    while IsRunning2 {
-    print("How many hours of sleep did you get on \(DayOfWeek)")
-    print("(To the nearest hour)")
-    let UserInput = readLine() ?? ""
-    let UserIntInput = Int(UserInput) ?? -1
 
-    if UserIntInput <= -1 {
+while isRunning {
+    print("How many hours of sleep did you get on \(dayOfWeek[whichDay])")
+    let userInput = readLine() ?? ""
+    let userDoubleInput = Double(userInput) ?? -1
+
+
+    func addSleepAndNextDay() {
+        hoursOfSleepEachDay[whichDay] = userDoubleInput
+        whichDay = whichDay + 1
+        print("\(userDoubleInput) has been added to record.")
+        print("")
+    }
+
+
+    if userDoubleInput <= -1 {
         print("Please make sure to enter a valid positive number.")
+        }
+    else if userDoubleInput > 24 {
+        print("\(userDoubleInput) is more than 24 hours which is the maximum time in a day.")   
+        print("Please try again.")
     }
-    else if UserIntInput > 24 {
-    print("There are more than \(UserIntInput)")   
+    else if userDoubleInput >= 16 {
+        print("Be aware that \(userDoubleInput) hours of sleep is abnormal.")
+        addSleepAndNextDay()
     }
+    else if userDoubleInput >= 0 && userDoubleInput <= 24 {
+        addSleepAndNextDay()
+    }
+    else {print("An error has occured, sorry for the inconvenience.")}
+
+
+    if whichDay == 7 {isRunning = false}
     }
 
 
-//note, turn ints into double so if user inputs 24.1 hours it says there are not that many hours in a day instead of it equaling to 24
+var sleepRating = ""
+if averageSleep < 0 {
+    sleepRating = "Error"
 }
+else if averageSleep < 6.0 {
+    sleepRating = "Poor"
+}
+else if averageSleep < 8.0 {
+    sleepRating = "Ok"
+}
+else if averageSleep > 8.0 && averageSleep < 24.0 {
+    sleepRating = "Good"
+}
+
+print("You got a total of \(totalHours) hours of sleep,")
+print("which averages to \(averageSleep) per day")
+print("")
+print("Your sleep rating is: \(sleepRating)")
+
 
 
 
