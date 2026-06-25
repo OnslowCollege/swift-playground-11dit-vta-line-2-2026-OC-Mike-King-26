ac94507ab73f25bb4ca0b5359921c2645515a7a5
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-06-25 03:08:34 +0000

    Small amounts of comments then testing!

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,38 +5,54 @@
 struct SwiftPlayground {
     static func main() {
 
+//Welcomes the user and asks what time limit should be set for each day.
 print("Welcome to screen time tracker.")
 print("")
 print("What is the hour limit set for each day?")
 print("Enter a number:")
 
 
+///Is used to keep the while loop running
 var isRunning1 = true
+
+///Sets the limit which used in the summary to check if the user doomscrolled too much or was all good
 var hourLimit = 0
+
 
 while isRunning1 {
 
+    //Turns user input into a constant for the loop to use.
+    //Also safely turns the input into -1 if it's nothing or invalid.
     let userInput = readLine() ?? ""
-    let hourLimitRequest = Int(userInput) ?? -1
+    let hourLimitRequest = Int(userInput) ?? -1 
 
 
+    //Checks if input is a negative number, if not, restarts again.
     if hourLimitRequest < 0 {
-        print("Please make sure to enter a positive whole number")
+        print("Please make sure to enter purely a positive whole number")
         print("Please try again.")
     }
+
+    //If input was 0, restarts because that is below the minimum time allowance.
     else if hourLimitRequest < 1 {
         print("The minimum time allowance is 1 hour.")
         print("Please try again.")
     }
+    //Checks if the input was more than there are hours in a day.
+    //If so, resets as that is invalid.
     else if hourLimitRequest > 24 {
         print("\(hourLimitRequest) hours is more than how many there are hours in a day.")
         print("And maximum time allowance is 5 hours.")
         print("Please try again.")
     }
+
+    //Restarts loop if the input is over the maximum time allowance
     else if hourLimitRequest > 5 {
         print("The maximum time allowance is 5 hours.")
         print("Please try again.")
     }
+
+    //Sets the input as time limit and finishes the loop.
     else {
         hourLimit = (hourLimitRequest)
         print("\(hourLimitRequest) has been set as the time allowance.")
@@ -46,23 +62,46 @@ while isRunning1 {
 }
 
 
+///Is a list of all of the days of the week for the program to print from.
 let dayOfWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+///Chooses which day of the week would be printed for the program.
 var whichDay = 0
 
-let socialMedias: [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "other"]
+///Is a list on all the different social medias the program would print from.
+let socialMedias: [String] = ["Facebook", "Snapchat", "Instagram", "Discord", "other", "test"]
+
+///Chooses which social media would be printed for the program.
+var whichSocialMedia = 0
 
 
-var hoursDoomscrolling = 0
+///Keeps the while loop running to collect all necesarry data for the summary.
 var isRunning2 = true
+
+
+///Stores data for the summary to use when enough data has been collected.
+var hoursDoomscrolling = 0
+
+///Stores data for hours spent in a given day.
 var hoursOnSocialMedias: [Int] = [0, 0, 0, 0, 0]
 
 
-
+//Keeps looping until the hours doomscrolled on all 7 days of the week gets collected.
 while isRunning2 {
-    var whichSocialMedia = 0
+
+    //Resets which social media would be shown back to the start
+    whichSocialMedia = 0
+
+
+    //Checks if all 7 days have been given screen time data on.
+    //If so, finishes the loop and moves onto summary
     if whichDay == 7 {
         isRunning2 = false
     }
+
+    /*Asks user how much scrolling they did on a certian day and starts the
+    data collection loop. This is all if all data hasn't been collected.
+    */
     else {  
         print("")
         print("On \(dayOfWeek[whichDay]), how many hours did you spend on:")
@@ -70,29 +109,51 @@ while isRunning2 {
     }
 
     while isRunning1 {
-        let totalHoursOnSocialMedias = hoursOnSocialMedias.reduce(0, +)
+
+        ///Turns the data of the hours user spent on social media into one number.
+        var totalHoursOnSocialMedias = hoursOnSocialMedias.reduce(0, +)
+
+        //Prints which social media the user needs to enter the info about.
         print("\(socialMedias[whichSocialMedia]):")
+
+        //Turns user input into a constant for the loop to use.
+        //Also safely turns the input into -1 if it's nothing or invalid.
         let userInput = readLine() ?? ""
         let hoursSpent = Int(userInput) ?? -1
 
+
+        //Checks if input is a negative number, if not, restarts again.
         if hoursSpent < 0 {
-            print("Time spent needs to be a positive number.")
+            print("Time spent needs to purely be a positive whole number.")
             print("Please Try again.")
-        }        
+        }   
+
+        //Checks if user entered more than 24 hours doomscrolling for one media.
+        //If so, tells the user to retry and redoes the media.
         else if hoursSpent > 24 {
             print("\(hoursSpent) is more than how many there are hours in a day.")
             print("Please try again.")
         }
+
+        //Checks if user inputed more hours doomscrolling than in a day.
+        //If they did, resets the day and tells the user to start again.
         else if hoursSpent + totalHoursOnSocialMedias > 24 {
             print("Your total hours on \(dayOfWeek[whichDay]) exceeds 24 hours.")
             print("You will need to redo this day.")
             print("")
             isRunning1 = false
         }
+
+        //Adds input to data collection and moves to next social media.
+        //Also restates what the total hours are for the day.
         else {
             hoursOnSocialMedias[whichSocialMedia] = hoursSpent
             whichSocialMedia = whichSocialMedia + 1
+            totalHoursOnSocialMedias = hoursOnSocialMedias.reduce(0, +)
         }
+
+        //checks if all data has been collected for the day
+        //if it has, provides brief summary and ends the loop making it the next day
         if whichSocialMedia == 5 {
             print("Overall on \(dayOfWeek[whichDay]) you spent \(totalHoursOnSocialMedias) hours doomscrolling.")
             whichDay = whichDay + 1
