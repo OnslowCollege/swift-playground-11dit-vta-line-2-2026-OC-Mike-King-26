4ebf05e2e080458c54c69df2b60ebb4bffd03432
OC-Mike-King-26 <mike.king@student.onslow.school.nz>
2026-08-04 09:32:51 +0000

    Aug 4, 2026, 9:32 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,250 +5,87 @@
 struct SwiftPlayground {
     static func main() {
 
-    //Welcomes the user and asks their work hours this week.
-    print("Welcome to Personal Finances Tracker")
-    print("")
-    print("Enter your work hours this week:")
+print("Welcome to the sleep tracker.")
+print("")
+print("Let's start by recording your sleep")
 
-    ///Variable keeps track of the user's work hours this week.
-    var workHours = 0
+// a bunch of variable which are being used by the program
+var whichDay = 0
+var hoursOfSleepEachDay: [Double] = [0, 0, 0, 0, 0, 0, 0]
+let daysOfWeek: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-    ///This variable is a general one used to start many while loops in the code.
-    var isRunning: Bool = true
 
-    //Repeats until the input for work hours is accepted and set.
-    while isRunning {
+var isRunning = true
 
-        //Receives input from user and turns the data into an integer.
-        //If the user inputs anything other than a number, workHoursRequest will be set to -1 instead.
-        let userInput = readLine() ?? ""
-        let workHoursRequest = Int(userInput) ?? -1
 
-        //Makes sure the user's input is a valid number for work hours.
-        //If not, restarts the loop and asks again.
-        if workHoursRequest < 0 {
-            print("Make sure to purely enter a valid whole number.")
-            print("Please try again:")
-        } else if workHoursRequest > 168 {
-            print("There are only 168 hours in a week.")
-            print("Please try again:")
-        } else if workHoursRequest > 20 {
-            print("Youth with part time jobs cannot exceed 20 hours per week.")
-            print("Please try again:")
+while isRunning {
+    print("How many hours of sleep did you get on \(daysOfWeek[whichDay])")
+    let userInput = readLine() ?? ""
+    let userDoubleInput = Double(userInput) ?? -1
 
-        //Sets the userinput and ends the loop.
-        } else {
-            workHours = workHoursRequest
-            isRunning = false
+
+    func addSleepAndNextDay() {
+        hoursOfSleepEachDay[whichDay] = userDoubleInput
+        whichDay = whichDay + 1
+        print("\(userDoubleInput) has been added to record.")
+        print("")
+    }
+
+
+    if userDoubleInput <= -1 {
+        print("Please make sure to enter a valid positive number.")
         }
+    else if userDoubleInput > 24 {
+        print("\(userDoubleInput) is more than 24 hours which is the maximum time in a day.")   
+        print("Please try again.")
     }
-
-    //Asks for hourly rate and makes a variable for the hourly rate to get put into.
-    print("Enter your hourly rate:")
-    var hourlyRate = 0
-
-    //Makes the while loop start repeating.
-    isRunning = true
-
-    //Repeats until the userinput for hourlyRate is accepted and set.
-    while isRunning {
-
-        //Receives input from user and turns the data into an integer.
-        //If the user inputs anything other than a number, hourlyRateRequest will be set to -1 instead.
-        let userInput = readLine() ?? ""
-        let hourlyRateRequest = Int(userInput) ?? -1
-
-        //Makes sure the userinput is a valid number for the hourly rate.
-        //If not, restarts the loop and asks for input again.
-        if hourlyRateRequest < 0 {
-            print("Make sure to purely enter a valid whole number.")
-            print("Please try again:")
-        } 
-        else if hourlyRateRequest < 19 {
-            print(
-                "Youth with part time jobs cannot earn less than $19 an hour as that is the minimum wage."
-            )
-            print("Please try again:")
-        } 
-        else if hourlyRateRequest > 24 {
-            print(
-                "Youth with part time jobs cannot earn more than $24 an hour as that is the adult minimum wage."
-            )
-            print("Please try again:")
-        } 
-
-        //Sets the userinput as the hourlyRate the ends the loop.
-        else {
-            hourlyRate = hourlyRateRequest
-            isRunning = false
-
-        }
+    else if userDoubleInput >= 16 {
+        print("Be aware that \(userDoubleInput) hours of sleep is abnormal.")
+        addSleepAndNextDay()
     }
-
-    ///Is the taxrate set for the code.
-    let taxRate = 0.15
-
-    ///Calculates the total money the user made before tax.
-    let totalMoneyBeforeTax = hourlyRate * workHours
-
-    ///Calculates the amount the user will be taxed on.
-    let taxAmount = Double(totalMoneyBeforeTax) * taxRate
-
-    ///Calculates the money the user has after taxes.
-    let totalMoneyAfterTax = Double(totalMoneyBeforeTax) - taxAmount
-
-    //Tells the user their total money before taxes, the amount they get taxed on, and the amount they have after taxes.
-    print("")
-    print("----")
-    print("You earn $\(totalMoneyBeforeTax).")
-    print("You pay $\(taxAmount) in income tax.")
-    print("You earn $\(totalMoneyAfterTax) TOTAL.")
-    print("----")
-    print("")
-
-    ///This variable is used to run the main while loop which adds expenses.
-    var listExpenses = false
-
-    //Makes the loop for asking if the user has expenses run.
-    isRunning = true
-
-    //Repeatedly asks the user if they have any expenses until the user puts in 'y', or 'n'.
-    while isRunning {
-
-        //Asks user if they have any expenses.
-        print("Do you have any expenses?")
-        print("(Y/N)")
-
-        //Turns the userinput into a constant and provides a fallback when unwrapping incase the user inputs nothing.
-        let userInput = readLine()?.lowercased() ?? ""
-        
-        //Checks if the user inputed a valid answer and loops until it is.
-        //Then answers accordingly.
-        if userInput != "y" && userInput != "n" {
-            print("Make sure to enter 'Y', or 'N'.")
-            print("Please try again:")
-        } else if userInput == "y" {
-            isRunning = false
-            listExpenses = true
-        } else if userInput == "n" {
-            isRunning = false
-        }
-        else {
-            print("An error has occured, please try again.")
-        }
+    else if userDoubleInput >= 0 && userDoubleInput <= 24 {
+        addSleepAndNextDay()
     }
-
-    var expenseNames: [String] = []
-    var expenseCosts: [Int] = []
-
-    while listExpenses {
-
-        isRunning = true
-
-        while isRunning {
-            print("")
-            print("Enter a name for your expense:")
-
-            let expenseName = readLine() ?? ""
-
-            if expenseName == "" {
-                print("Make sure to enter a valid expense name.")
-                print("Please try again:")
-            } else {
-                expenseNames.append(expenseName)
-                print("Enter '\(expenseName)' cost per week:")
-                isRunning = false
-            }
-        }
+    else {print("An error has occured, sorry for the inconvenience.")}
 
 
-        isRunning = true
-
-        while isRunning {
-            let expenseCostRequest = readLine() ?? ""
-            let expenseCost = Int(expenseCostRequest) ?? -1
-
-            if expenseCost < 0 {
-                print("Make sure to purely enter a valid whole number.")
-                print("Please try again:")
-            }
-            else {
-                expenseCosts.append(expenseCost)
-                isRunning = false
-            }
-        }
-    
-        print("Add another expense?")
-        print("(Y/N):")
-        isRunning = true
-
-        while isRunning {
-            let userInput = readLine()?.lowercased() ?? ""
-
-            if userInput != "y" && userInput != "n" {
-                print("Please make sure to enter 'Y', or 'N'.")
-            }
-            else if userInput == "y" {
-                isRunning = false
-            }
-            else if userInput == "n" {
-                isRunning = false
-                listExpenses = false
-            }
-            else {
-                print("An error has occured, please try again.")
-            }
-        }
-        
-
+    if whichDay == 7 {isRunning = false}
     }
 
 
-    var areThereExpenses = false
+let totalHours = hoursOfSleepEachDay.reduce(0, +)
+let averageSleep = Int(totalHours / 7)
 
-    if expenseNames.count > 0 {
-        areThereExpenses = true
-    }
 
-    var totalMoneyAfterExpenses = totalMoneyAfterTax
+var sleepRating = ""
+if averageSleep < 0 {
+    sleepRating = "Error"
+}
+else if averageSleep < 6 {
+    sleepRating = "Poor"
+}
+else if averageSleep < 8 {
+    sleepRating = "Ok"
+}
+else {
+    sleepRating = "Good"
+}
 
-    if areThereExpenses == true {
-    var whichExpense = 0
-    print("----")
-    print("$\(totalMoneyAfterTax)")
-    for _ in expenseNames {
-    print("- $\(expenseCosts[whichExpense]) for \(expenseNames[whichExpense])")
-    totalMoneyAfterExpenses = totalMoneyAfterExpenses - Double(expenseCosts[whichExpense])
-    whichExpense = whichExpense + 1
-    }
-    print("----")
-    }
 
-    else {
-        print("----")
-        print("$\(totalMoneyAfterTax)")
-        print("- No expenses")
-        print("----")
-    }
+var dayCount = 0
 
-    var isInDebt = false
+for dayOfWeek in daysOfWeek {
+    print("Your sleep on \(dayOfWeek) was: \(hoursOfSleepEachDay[dayCount]) hours.")
+    dayCount = dayCount + 1
+}
 
-    if totalMoneyAfterExpenses < 0 {
-        print("= -$\(totalMoneyAfterExpenses)")
-        isInDebt = true
-    }
-    else {
-        print("= $\(totalMoneyAfterExpenses)")
-    }
 
-    print("")
+print("")
+print("You got a total of \(totalHours) hours of sleep,")
+print("which averages to around \(averageSleep) hours per day")
+print("")
+print("Your sleep rating is: \(sleepRating)")
 
-    if isInDebt {
-        print("You are in debt this week. please reduce your expenses.")
-    }
-    else {
-        print("You can afford your expenses this week.")
-    }
 
     }
 }
